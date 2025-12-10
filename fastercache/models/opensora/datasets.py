de13213e7a898fd13341f7c3a2409d78719d5d81
Kate Zhang <kate.a.zhang@gmail.com>
2025-12-10 05:17:33 -0600

    Removed video_codec parameter

--- fastercache/models/opensora/datasets.py
+++ fastercache/models/opensora/datasets.py
@@ -520,7 +520,7 @@ def save_sample(x, save_path=None, fps=8, normalize=True, value_range=(-1, 1), f
             x.sub_(low).div_(max(high - low, 1e-5))
 
         x = x.mul(255).add_(0.5).clamp_(0, 255).permute(1, 2, 3, 0).to("cpu", torch.uint8)
-        write_video(save_path, x, fps=fps, video_codec="h264")
+        write_video(save_path, x, fps=fps)
     if verbose:
         print(f"Saved to {save_path}")
     return save_path
